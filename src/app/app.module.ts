ab4d0b4d366dd4a60dee396b4663eb27dcd6c531
amadeoterri-hogent <amadeo.terriere@student.hogent.be>
2021-02-24 20:42:58 +0100

    lesson 1 angular basics: - created ingredient & recipe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { RecipeModule } from './recipe/recipe.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    IngredientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RecipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ingredient/ingredient.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient',
+  templateUrl: './ingredient.component.html',
+  styleUrls: ['./ingredient.component.css']
+})
+export class IngredientComponent implements OnInit {
+
+  @Input() name: string = 'biatch';
+
+  constructor() {
+    console.log("biatch");
+  }
+
+  ngOnInit(): void {
+  }
+
+}
